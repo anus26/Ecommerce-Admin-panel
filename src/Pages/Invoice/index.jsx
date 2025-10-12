3c3637fd612c84c7e156e5fbd020e6623ac3b94b
anus26 <anusraza268@gmail.com>
2025-10-12 06:11:06 -0700

    invoice

--- src/Pages/Invoice/index.jsx
+++ src/Pages/Invoice/index.jsx
@@ -5,12 +5,14 @@ import { IoArrowUp } from "react-icons/io5";
 import { BsArrowBarUp } from "react-icons/bs";
 import axios from "axios";
 import { RiDeleteBin5Line } from "react-icons/ri";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Invoice = () => {
   const [count, setCount] = useState(1);
   const [products, setProdcut] = useState([]);
+  const {_id}=useParams
   const [filtered, setFilteredProducts] = useState([]);
+    const [invoice, setInvoice] = useState(null);
   const [showfilter, setShowFilter] = useState(false);
   const navigate=useNavigate()
   const [Data, setData] = useState({
@@ -150,7 +152,8 @@ const Invoice = () => {
   }
     useEffect(() => {
       fetchData();
-    }, []);
+      fetchDatas()
+    }, [_id]);
     useEffect(()=>{
       if (Data.ProductName.trim()==="") {
         setFilteredProducts([])
@@ -181,6 +184,20 @@ const handledelete=(index)=>{
   }))
 
 }
+
+  const fetchDatas = async () => {
+    try {
+      const res = await axios.get(`http://localhost:5000/api/v1/oneinvoice/${_id}`);
+      console.log("✅ Invoice Data:", res.data.invoice);
+      setFormData(res.data.invoice);
+    } catch (error) {
+      console.error("❌ Fetch Error:", error.message);
+    }
+  };
+
+
+
+ 
 
 
   return (
@@ -497,10 +514,87 @@ const handledelete=(index)=>{
                   </button>
                 </div>
             </form>
-            <button className="border bg-white hover:textt">Preview</button>
+            <button className="border bg-white hover:textt">Preview
+              {/* {
+             <div className="bg-white border-gray rounded-lg ">
+        
+    
+                  <div className="flex justify-between m-5">
+                    <div>
+                      <h2 className="font-semibold text-lg text-textt">Form</h2>
+                      <p>{invoice.CustomerName}</p>
+                      <p>{invoice.CustomerAddress}</p>
+                         <p>Issued: {invoice.IssueDate}</p>
+                    </div>
+                    <div>
+                      <h2 className="font-semibold text-lg">To:</h2>
+                      <p className="text-textt">Issued: {invoice.IssueDate}</p>
+                      <p className="text-textt">Due: {invoice.DueDate}</p>
+                    </div>
+                  </div>
+            
+            
+               
+                    <div className="border border-gray m-5"> 
+            <div className="flex border-b border-gray bg-gray p-3 rounded-sm" >
+            
+                        <h1 className="w-[5%]">S.No#</h1>
+                        <h1 className="w-[20%] ">Product</h1>
+                        <h1 className=" w-[20%] ">Quantity</h1>
+                        <h1 className="w-[20%] ">Unit Cost</h1>
+                        <h1 className=" w-[20%] ">Discount</h1>
+                        <h1 className=" w-[5%]">Total</h1>
+                     
+                  
+                        </div>
+                      {invoice.Products.map((p, index) => (
+                          <div key={index} className="flex border-b border-gray p-2">
+                          <h1 className="w-[5%]">{index + 1}</h1>
+                          <h1 className="w-[20%]">{p.ProductName}</h1>
+                          <h1 className="w-[20%]">{p.StockQuantity}</h1>
+                          <h1 className="w-[20%]">{p.Price}</h1>
+                          <h1 className="w-[20%]">{p.  Discount}</h1>
+                          <h1 className="w-[5%]">{p.Price*p.StockQuantity/p.Discount}</h1>
+                        </div>
+            
+                      ))}
+            
+               
+                                      </div>
+            <div className="flex justify-end mt-6">
+              <div className="bg-gray-50 p-5 rounded-lg  w-[300px]">
+                <h1 className="text-lg font-semibold mb-3  pb-2 text-left">
+                  Order Summary
+                </h1>
+            
+                <div className="space-y-2 text-sm text-gray-700">
+                  <div className="flex justify-between">
+                    <span>Sub Total</span>
+                    <span className="font-medium">Rs. {totalPrice}</span>
+                  </div>
+            
+                  <div className="flex justify-between">
+                    <span>VAT (10%)</span>
+                    <span className="font-medium">Rs. {totalvat}</span>
+                  </div>
+            
+                  <div className=" pt-2 mt-2 flex justify-between text-base font-semibold">
+                    <span>Total</span>
+                    <span className="text-primary">Rs. {totalPrice + totalvat}</span>
+                  </div>
+                </div>
               </div>
+            </div>
+         
+            
+                  
+            
+                          
+                                      </div>} */}
+                                      </button>
           </div>
         </div>
+              </div>
       </section>
     </>
   );
