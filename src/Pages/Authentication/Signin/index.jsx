b3192eb99446634f47543c10fde5485c30f2b2c0
anus26 <anusraza268@gmail.com>
2026-01-13 18:40:23 -0800

    modified

--- src/Pages/Authentication/Signin/index.jsx
+++ src/Pages/Authentication/Signin/index.jsx
@@ -103,19 +103,19 @@ try {
 
     </div>
   </div>
-<div className='w-[50%] hidden lg:block'>
-    <div className="back flex  items-center justify-center gap-4">
-      <img
-        src="/images/control-panel.png"
-        alt="control"
-        className="w-[12%]"
-      />
-      <h1 className="text-white text-4xl font-bold">
-        AdminPanel
-      </h1>
-    </div>
-
+<div className="w-[50%] hidden lg:block">
+  <div className="back h-screen flex items-center justify-center gap-4">
+    <img
+      src="/images/control-panel.png"
+      alt="control"
+      className="w-[12%]"
+    />
+    <h1 className="text-white text-4xl font-bold">
+      AdminPanel
+    </h1>
+  </div>
 </div>
+
 
 </section>
     </>
