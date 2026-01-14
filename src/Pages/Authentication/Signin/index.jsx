261b74360d9a2ee6d0fea2582cf14629525bbbb2
anus26 <anusraza268@gmail.com>
2026-01-13 22:44:18 -0800

    resolve style.css

--- src/Pages/Authentication/Signin/index.jsx
+++ src/Pages/Authentication/Signin/index.jsx
@@ -4,7 +4,8 @@ import { RiTwitterXLine } from "react-icons/ri";
 import { BsEye } from "react-icons/bs";
 import { BsEyeSlash } from "react-icons/bs";
 import { IoIosArrowBack } from "react-icons/io";
-import './style.css'
+import './style.css';
+
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 import { AppContext } from '../../../Context/AppContext';
